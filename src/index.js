a9710de16bc19a9b4fc1975f7f9ee8f6854f970d
webcodur <webcodur@gmail.com>
2024-07-03 15:09:42 +0900

    update

--- src/index.js
+++ src/index.js
@@ -8,9 +8,8 @@ import './index.css';
 const container = document.getElementById('root');
 const root = createRoot(container);
 
-// BrowserRouter에 basename 속성을 추가하여 /difference 경로를 설정
 root.render(
-	<BrowserRouter basename="/difference">
+	<BrowserRouter basename="/game-difference">
 		<App />
 	</BrowserRouter>
 );
